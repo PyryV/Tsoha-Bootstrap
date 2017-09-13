2e2778d1498d648db7d30a2f9ad0a77352746a26
PyryV <pyry.v@hotmail.fi>
2017-09-13 20:04:51 +0300

    tietokantataulut luotu

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,15 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+--Pelaaja-taulun testidata
+INSERT INTO Pelaaja(nimi, joukkue, taso) 
+VALUES ('Sidney Crosby', 'Winnipeg Penguins', 95);
+
+INSERT INTO Pelaaja(nimi, joukkue, taso) 
+VALUES ('Jamie Benn', 'Dallas Stars', 94);
+
+--Kayttaja-taulun testidata
+INSERT INTO Kayttaja(nimi, password) VALUES ('Pekka', 'abc123');
+INSERT INTO Kayttaja(nimi, password) VALUES ('Janne', 'salasana');
+
+--Joukkue-taulun testidata
+INSERT INTO Joukkue(nimi) VALUES ('Ultimate team');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,21 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Pelaaja(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(50) NOT NULL,
+    joukkue varchar(50) NOT NULL,
+    taso INTEGER 
+);
+
+CREATE TABLE Kayttaja(
+    id SERIAL PRIMARY KEY,
+    nimi varchar(20) NOT NULL,
+    password varchar(50) NOT NULL
+);
+
+CREATE TABLE Joukkue(
+    pelaaja INTEGER,
+    omistaja INTEGER,
+    nimi varchar(20),
+    FOREIGN KEY (pelaaja) REFERENCES Pelaaja(id),
+    FOREIGN KEY (omistaja) REFERENCES Kayttaja(id)
+);
