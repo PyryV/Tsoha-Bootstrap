b237ff859a05cd5592ed472214276569a04253b1
PyryV <pyry.v@hotmail.fi>
2017-09-14 17:00:35 +0300

    Html-näkymät lisätty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -10,6 +10,26 @@
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+    
+    public static function login(){
+        View::make('suunnitelmat/login.html');
+    }
+    
+    public static function joukkueet(){
+        View::make('suunnitelmat/joukkueet.html');
+    }
+    
+    public static function esittely_joukkue(){
+        View::make('suunnitelmat/esittely_joukkue.html');
+    }
+    
+    public static function muokkaus_joukkue(){
+        View::make('suunnitelmat/muokkaus_joukkue.html');
+    }
+    
+    public static function muokkaus_pelaaja(){
+        View::make('suunnitelmat/muokkaus_pelaaja.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,23 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $routes->get('/joukkueet', function() {
+    HelloWorldController::joukkueet();
+  });
+  
+  $routes->get('/joukkueet/1', function() {
+    HelloWorldController::esittely_joukkue();
+  });
+  
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
+  
+  $routes->get('/joukkueet/1/muokkaus', function() {
+    HelloWorldController::muokkaus_joukkue();
+  });
+  
+  $routes->get('/joukkueet/1/pelaajamuokkaus', function() {
+    HelloWorldController::muokkaus_pelaaja();
+  });
